d1c05fe3b4f86ecf7321e2a6fd64798c8db7750e
Rustam Akhmedzianov <ahmedzyanov.r.1997@gmail.com>
2023-03-03 14:38:40 +0300

    add BaseSchema & NumberSchema

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,2 +1,37 @@
-package hexlet.code.schemas;public class NumberSchema {
+package hexlet.code.schemas;
+
+import java.util.function.Predicate;
+
+public class NumberSchema extends BaseSchema {
+
+    public NumberSchema() {
+        Predicate<Object> isNumber = x -> x instanceof Integer;
+        addValid(isNumber);
+    }
+
+    public NumberSchema positive() {
+        Predicate<Integer> isPositive = x -> x > 0;
+        addValid(isPositive);
+        return this;
+    }
+
+    public void range(int min, int max) {
+        Predicate<Integer> range = x -> x <= max && x >= min;
+        addValid(range);
+    }
+
+    public boolean isValid(Object obj) {
+        if (!isRequired() && (obj == null)) {
+            return true;
+        } else if (isRequired() && (obj == null)) {
+            return false;
+        } else {
+            for (Predicate pred : getValid()) {
+                if (!pred.test(obj)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
