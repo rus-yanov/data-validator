cc2ad721e5d28f014e83b1fcd7bf892360e4d385
Rustam Akhmedzianov <ahmedzyanov.r.1997@gmail.com>
2023-03-03 12:46:26 +0300

    add StringSchema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,20 +1,48 @@
 package hexlet.code.schemas;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class StringSchema {
 
-    public void required() {
+    private List<Predicate> validStuff = new ArrayList<>();
+    private boolean isRequired = false;
 
+    public StringSchema() {
+        Predicate<Object> isString = x -> x instanceof String;
+        validStuff.add(isString);
     }
 
-    public boolean minLength(String str, int num) {
+    public void required() {
+        this.isRequired = true;
+    }
 
+    public StringSchema minLength(int num) {
+        Predicate<String> minLength = x -> x.length() >= num;
+        validStuff.add(minLength);
+        return this;
     }
 
     public StringSchema contains(String str) {
-
+        Predicate<String> contains = x -> x.contains(str);
+        validStuff.add(contains);
+        return this;
     }
 
     public boolean isValid(Object obj) {
 
+        if (!isRequired && (obj == null || obj.equals(""))) {
+            return true;
+        } else if (isRequired && (obj == null || obj.equals(""))) {
+            return false;
+        } else {
+            for (Predicate pred : validStuff) {
+                if (!pred.test(obj)) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
